2582ef08f6d9d7b70af4feae75a8793524ef4bc2
Marek Pšenka <psenka@edhouse.cz>
2024-12-01 13:18:19 +0100

    Day 01

--- day01a.py
+++ day01a.py
@@ -1,6 +1,16 @@
 import fileinput
 
-input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+def parse_pairs(line: str) -> tuple[int, int]:
+    first_str, second_str = line.split()
+    return (int(first_str), int(second_str))
+
+
+input = map(parse_pairs, map(str.rstrip, fileinput.input()))
+
+left, right = zip(*input)
+
+matched = zip(sorted(left), sorted(right))
+diffs = map(lambda p: abs(p[0] - p[1]), matched)
+
+print(sum(diffs))
--- day01b.py
+++ day01b.py
@@ -1,6 +1,17 @@
 import fileinput
+from collections import Counter
 
-input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+def parse_pairs(line: str) -> tuple[int, int]:
+    first_str, second_str = line.split()
+    return (int(first_str), int(second_str))
+
+
+input = map(parse_pairs, map(str.rstrip, fileinput.input()))
+
+left, right = zip(*input)
+counts = Counter(right)
+
+scores = map(lambda n: n * counts[n], left)
+
+print(sum(scores))
