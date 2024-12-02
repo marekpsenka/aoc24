e649a13783710d099aee1c054c101a50e65ab412
Marek Pšenka <psenka@edhouse.cz>
2024-12-02 21:30:12 +0100

    Day 02

--- day02a.py
+++ day02a.py
@@ -2,5 +2,25 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
+safe_count = 0
+
 for line in input:
-    print(line)
+    levels = list(map(int, line.split()))
+    safe_increasing = True
+    for i in range(len(levels) - 1):
+        diff = levels[i + 1] - levels[i]
+        if diff < 1 or diff > 3:
+            safe_increasing = False
+            break
+
+    safe_decreasing = True
+    for i in range(len(levels) - 1):
+        diff = levels[i] - levels[i + 1]
+        if diff < 1 or diff > 3:
+            safe_decreasing = False
+            break
+
+    if safe_increasing or safe_decreasing:
+        safe_count += 1
+
+print(safe_count)
