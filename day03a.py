e8295a34772ae7d52ba3b31fd57a096ae60ef960
Marek Pšenka <psenka@edhouse.cz>
2024-12-03 16:11:53 +0100

    Day 03

--- day03a.py
+++ day03a.py
@@ -1,6 +1,15 @@
 import fileinput
+from re import finditer, Match
 
 input = map(str.rstrip, fileinput.input())
+result = 0
+
+
+def mul(m: Match) -> int:
+    return int(m.group(1)) * int(m.group(2))
+
 
 for line in input:
-    print(line)
+    result += sum(map(mul, finditer(r"mul\((\d{1,3}),(\d{1,3})\)", line)))
+
+print(result)
--- day03b.py
+++ day03b.py
@@ -1,6 +1,27 @@
 import fileinput
+from re import finditer, Match
 
 input = map(str.rstrip, fileinput.input())
+result = 0
 
+
+def mul(m: Match) -> int:
+    return int(m.group(1)) * int(m.group(2))
+
+
+active = True
 for line in input:
-    print(line)
+    all_matches: list[Match] = [
+        m
+        for m in finditer(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)", line)
+    ]
+
+    for m in all_matches:
+        if m.group(0).startswith("do()"):
+            active = True
+        elif m.group(0).startswith("don't()"):
+            active = False
+        elif m.group(0).startswith("mul") and active:
+            result += mul(m)
+
+print(result)
