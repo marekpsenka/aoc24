bc61d4cb2f885c1adf3d1840e6f58ec22d7a0554
Marek Pšenka <psenka@edhouse.cz>
2024-12-07 13:53:36 +0100

    Day 07

--- day07a.py
+++ day07a.py
@@ -2,5 +2,29 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
+
+def f(test: int, current: int, i: int, numbers: list[int]) -> int:
+    if current > test:
+        return 0
+
+    if i == len(numbers):
+        if test == current:
+            return 1
+        else:
+            return 0
+    else:
+        return f(test, current + numbers[i], i + 1, numbers) + f(
+            test, current * numbers[i], i + 1, numbers
+        )
+
+
+total = 0
+
 for line in input:
-    print(line)
+    test_str, numbers_str = line.split(": ")
+    numbers = list(map(int, numbers_str.split()))
+    test = int(test_str)
+    if f(test, numbers[0], 1, numbers) > 0:
+        total += test
+
+print(total)
