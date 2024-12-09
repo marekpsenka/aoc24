82006acc1c8a17a8d54a73d5ef7ed7bf014d0e27
Marek Pšenka <psenka@edhouse.cz>
2024-12-09 16:41:04 +0100

    Day 09

--- day09a.py
+++ day09a.py
@@ -1,6 +1,41 @@
 import fileinput
+from collections import deque
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+files: deque[int] = deque()
+spaces: list[int] = list()
+
+for i, c in enumerate(next(input)):
+    if i % 2 == 0:
+        files.append(int(c))
+    else:
+        spaces.append(int(c))
+
+spaces.reverse()
+
+checksum = 0
+file_index = 0
+pos = 0
+while len(files) > 0:
+    for _ in range(files.popleft()):
+        checksum += pos * file_index
+        pos += 1
+
+    to_fill = spaces.pop()
+    while to_fill > 0 and len(files) > 0:
+        file_size = files.pop()
+        if to_fill >= file_size:
+            for _ in range(file_size):
+                checksum += pos * (len(files) + 1 + file_index)
+                pos += 1
+            to_fill -= file_size
+        else:  # to_fill < file_size
+            for _ in range(to_fill):
+                checksum += pos * (len(files) + 1 + file_index)
+                pos += 1
+            files.append(file_size - to_fill)
+            to_fill = 0
+    file_index += 1
+
+print(checksum)
--- day09b.py
+++ day09b.py
@@ -1,6 +1,41 @@
 import fileinput
 
+
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+files: list[tuple[int, int]] = list()
+spaces: list[tuple[int, int]] = list()
+
+pos = 0
+for i, c in enumerate(next(input)):
+    size = int(c)
+    if i % 2 == 0:
+        files.append((pos, size))
+    else:
+        spaces.append((pos, size))
+    pos += size
+
+checksum = 0
+while len(files) > 0:
+    file_loc, file_size = files.pop()
+    space_found = False
+    for i in range(len(spaces)):
+        space_loc, space_size = spaces[i]
+        if space_loc > file_loc:
+            break
+        if space_size >= file_size:
+            pos = space_loc
+            for _ in range(file_size):
+                checksum += pos * len(files)
+                pos += 1
+            space_found = True
+            spaces[i] = (pos, space_size - file_size)
+            break
+    if not space_found:
+        pos = file_loc
+        for _ in range(file_size):
+            checksum += pos * len(files)
+            pos += 1
+
+
+print(checksum)
