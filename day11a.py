4fc8b5a976460ee186f106f60a85d9bbe109429f
Marek Pšenka <psenka@edhouse.cz>
2024-12-11 10:11:54 +0100

    Day 11

--- day11a.py
+++ day11a.py
@@ -1,6 +1,26 @@
 import fileinput
+from collections import defaultdict
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+stones: defaultdict[int, int] = defaultdict(int)
+
+for mark_str in next(input).split():
+    stones[int(mark_str)] = 1
+
+
+for _ in range(25):
+    next_stones: defaultdict[int, int] = defaultdict(int)
+    for mark, count in stones.items():
+        mark_str = str(mark)
+        if mark == 0:
+            next_stones[1] += count
+        elif len(mark_str) % 2 == 0:
+            next_stones[int(mark_str[0 : len(mark_str) // 2])] += count
+            next_stones[int(mark_str[len(mark_str) // 2 :])] += count
+        else:
+            next_stones[mark * 2024] += count
+
+    stones = next_stones
+
+print(sum(stones.values()))
