979a4739ff17d71cb640c93a80ff9732aded3025
Marek Pšenka <psenka@edhouse.cz>
2024-12-14 15:09:18 +0100

    Day 14

--- day14a.py
+++ day14a.py
@@ -1,6 +1,48 @@
 import fileinput
+from re import search
 
-input = map(str.rstrip, fileinput.input())
+Vec2 = tuple[int, int]
+
+
+def extract_pos_vel(s: str) -> tuple[Vec2, Vec2]:
+    m = search(r"p\=(\d+),(\d+) v\=(-?\d+),(-?\d+)", s)
+    assert m is not None
+    return (
+        (int(m.group(1)), int(m.group(2))),
+        (int(m.group(3)), int(m.group(4))),
+    )
+
+
+def sum_scaled(v: Vec2, w: Vec2, s: Vec2) -> Vec2:
+    return (s[0] * v[0] + s[1] * w[0], s[0] * v[1] + s[1] * w[1])
+
+
+input = map(extract_pos_vel, map(str.rstrip, fileinput.input()))
+
+bound_x = 101
+bound_y = 103
+
+robots_pp = 0
+robots_pm = 0
+robots_mm = 0
+robots_mp = 0
+
 
 for line in input:
-    print(line)
+    p, v = line
+    npx, npy = sum_scaled(p, v, (1, 100))
+    npx = npx % bound_x
+    npy = npy % bound_y
+
+    if npx < bound_x // 2:
+        if npy < bound_y // 2:
+            robots_mm += 1
+        elif npy > bound_y // 2:
+            robots_mp += 1
+    elif npx > bound_x // 2:
+        if npy < bound_y // 2:
+            robots_pm += 1
+        elif npy > bound_y // 2:
+            robots_pp += 1
+
+print(robots_mp * robots_mm * robots_pm * robots_pp)
--- day14b.py
+++ day14b.py
@@ -1,6 +1,54 @@
 import fileinput
+from re import search
 
-input = map(str.rstrip, fileinput.input())
+Vec2 = tuple[int, int]
 
-for line in input:
-    print(line)
+
+def extract_pos_vel(s: str) -> tuple[Vec2, Vec2]:
+    m = search(r"p\=(\d+),(\d+) v\=(-?\d+),(-?\d+)", s)
+    assert m is not None
+    return (
+        (int(m.group(1)), int(m.group(2))),
+        (int(m.group(3)), int(m.group(4))),
+    )
+
+
+def sum_scaled(v: Vec2, w: Vec2, s: Vec2) -> Vec2:
+    return (s[0] * v[0] + s[1] * w[0], s[0] * v[1] + s[1] * w[1])
+
+
+def neighbors_4_naive(p: Vec2) -> list[Vec2]:
+    i, j = p
+    return [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]
+
+
+def num_connected(ps: set[Vec2]) -> int:
+    result = 0
+    for p in ps:
+        if any(map(lambda n: n in ps, neighbors_4_naive(p))):
+            result += 1
+
+    return result
+
+
+input = list(map(extract_pos_vel, map(str.rstrip, fileinput.input())))
+
+bound_x = 101
+bound_y = 103
+
+i = 0
+while True:
+    points: set[Vec2] = set()
+    for line in input:
+        p, v = line
+        npx, npy = sum_scaled(p, v, (1, i))
+        npx = npx % bound_x
+        npy = npy % bound_y
+        points.add((npx, npy))
+
+    nc = num_connected(points)
+    if nc > len(input) // 2:
+        print(i)
+        break
+
+    i += 1
