4cead46be9bbf62bdbaabd41e82786d143256f38
Marek Pšenka <psenka@edhouse.cz>
2024-12-19 13:53:50 +0100

    Day 19

--- day19a.py
+++ day19a.py
@@ -1,6 +1,34 @@
 import fileinput
+from dataclasses import dataclass
 
 input = map(str.rstrip, fileinput.input())
+towels: set[str] = set(next(input).split(", "))
+assert not next(input)
 
+
+@dataclass
+class State:
+    consumed: int
+
+
+def is_possible(pattern: str) -> bool:
+    q: list[State] = [State(0)]
+    while len(q) > 0:
+        s = q.pop()
+        if s.consumed == len(pattern) - 1:
+            return True
+        i = s.consumed
+        while i < len(pattern):
+            if pattern[s.consumed : i] in towels:
+                q.append(State(i))
+            i += 1
+
+    return False
+
+
+possible = 0
 for line in input:
-    print(line)
+    if is_possible(line):
+        possible += 1
+
+print(possible)
--- day19b.py
+++ day19b.py
@@ -1,6 +1,31 @@
 import fileinput
 
 input = map(str.rstrip, fileinput.input())
+towels: set[str] = set(next(input).split(", "))
+assert not next(input)
 
+cache: dict[str, int] = dict()
+
+
+def possibilities(pattern: str) -> int:
+    if not pattern:
+        return 1
+
+    if pattern in cache:
+        return cache[pattern]
+
+    result = 0
+    for i in range(1, len(pattern) + 1):
+        if pattern[:i] in towels:
+            result += possibilities(pattern[i:])
+
+    assert pattern not in cache
+    cache[pattern] = result
+    return result
+
+
+total_possibilities = 0
 for line in input:
-    print(line)
+    total_possibilities += possibilities(line)
+
+print(total_possibilities)
