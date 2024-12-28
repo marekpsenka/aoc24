aa63de9607366d572a41d42af6c8753dcb29032b
Marek Pšenka <psenka@edhouse.cz>
2024-12-28 20:05:44 +0100

    Day 23

--- day23a.py
+++ day23a.py
@@ -1,6 +1,31 @@
 import fileinput
+from collections import defaultdict
 
 input = map(str.rstrip, fileinput.input())
 
+connections: defaultdict[str, set[str]] = defaultdict(set)
+
 for line in input:
-    print(line)
+    left, right = line.split("-")
+    connections[left].add(right)
+    connections[right].add(left)
+
+result: set[tuple[str, str, str]] = set()
+
+
+def stupid_sort(c1: str, c2: str, c3: str) -> tuple[str, str, str]:
+    temp = [c1, c2, c3]
+    r1, r2, r3 = sorted(temp)
+    return (r1, r2, r3)
+
+
+for c1, cons in connections.items():
+    for c2 in cons:
+        for c3 in cons.intersection(connections[c2]):
+            if not any(map(lambda c: c.startswith("t"), [c1, c2, c3])):
+                continue
+            triple = stupid_sort(c1, c2, c3)
+            if triple not in result:
+                result.add(triple)
+
+print(len(result))
--- day23b.py
+++ day23b.py
@@ -1,6 +1,51 @@
 import fileinput
+from collections import defaultdict
+from dataclasses import dataclass
+from collections import deque
 
 input = map(str.rstrip, fileinput.input())
 
+connections: defaultdict[str, set[str]] = defaultdict(set)
+
 for line in input:
-    print(line)
+    left, right = line.split("-")
+    connections[left].add(right)
+    connections[right].add(left)
+
+
+@dataclass
+class State:
+    clique: list[str]
+    candidate: str
+
+
+def gen_key(ss: list[str]) -> str:
+    return ",".join(sorted(ss))
+
+
+visited: set[str] = set()
+
+max_clique: list[str] = list()
+for c1, cons in connections.items():
+    q: deque[State] = deque(State([c1], c2) for c2 in cons)
+    while len(q) > 0:
+        s = q.popleft()
+        for_key = s.clique.copy()
+        for_key.append(s.candidate)
+        key = gen_key(for_key)
+        if key in visited:
+            continue
+        else:
+            visited.add(key)
+
+        if not all(map(lambda c: s.candidate in connections[c], s.clique)):
+            if len(s.clique) > len(max_clique):
+                max_clique = s.clique
+            continue
+
+        for c in connections[s.candidate]:
+            new_clique = s.clique.copy()
+            new_clique.append(s.candidate)
+            q.append(State(new_clique, c))
+
+print(gen_key(max_clique))
