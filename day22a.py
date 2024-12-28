79c6f900047e519a32acc2daf9a7ad172e6fe341
Marek Pšenka <psenka@edhouse.cz>
2024-12-28 14:22:57 +0100

    Day 22

--- day22a.py
+++ day22a.py
@@ -2,5 +2,17 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
+total = 0
 for line in input:
-    print(line)
+    secret = int(line)
+    for _ in range(2000):
+        secret = (secret * 64) ^ secret
+        secret = secret % 16777216
+        secret = (secret // 32) ^ secret
+        secret = secret % 16777216
+        secret = (secret * 2048) ^ secret
+        secret = secret % 16777216
+
+    total += secret
+
+print(total)
--- day22b.py
+++ day22b.py
@@ -1,6 +1,42 @@
 import fileinput
+from collections import defaultdict
 
 input = map(str.rstrip, fileinput.input())
 
+
+def ones_digit(n: int) -> int:
+    return int(str(n)[-1])
+
+
+bananas: defaultdict[tuple[int, int, int, int], int] = defaultdict(int)
+total = 0
 for line in input:
-    print(line)
+    secret = int(line)
+    last_digit = ones_digit(secret)
+    data: list[tuple[int, int]] = []
+    for _ in range(2000):
+        secret = (secret * 64) ^ secret
+        secret = secret % 16777216
+        secret = (secret // 32) ^ secret
+        secret = secret % 16777216
+        secret = (secret * 2048) ^ secret
+        secret = secret % 16777216
+        digit = ones_digit(secret)
+        data.append((digit, digit - last_digit))
+        last_digit = digit
+
+    seen: set[tuple[int, int, int, int]] = set()
+    for i in range(3, 2000):
+        this_tuple = (
+            data[i - 3][1],
+            data[i - 2][1],
+            data[i - 1][1],
+            data[i][1],
+        )
+        if this_tuple in seen:
+            continue
+        seen.add(this_tuple)
+        bananas[this_tuple] += data[i][0]
+
+
+print(max(bananas.values()))
